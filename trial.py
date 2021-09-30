04af9ed88f9c2ebaf78e7dabd91a3c3194074d2f
Siddharth <siddharth.chaberia26@gmail.com>
2021-09-30 11:34:34 +0530

    modified trial.py

--- trial.py
+++ trial.py
@@ -1,3 +1,9 @@
-print("HEHEHEHEHEHEHE")
-print(2*3)
+from flask import Flask
+app=Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello.World!'
+
+if __name__ =="__main__":
+    app.run(debug=True)
